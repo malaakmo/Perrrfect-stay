ffbcb5f8bd57ddf7e933e93a1427d2058b34d9b9
User <User@DESKTOP-DAJT9J5>
2026-05-20 15:56:37 +0200

    Add project files.

---
+++ Cat.cs
@@ -0,0 +1,15 @@
+﻿namespace PurrfectStayAPI
+{
+    public class Cat
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string breed { get; set; }
+        public int age_years { get; set; }
+        public string gender { get; set; }
+        public string personality { get; set; }
+        public string photo { get; set; }
+        public string health_status { get; set; }
+        public string adoption_status { get; set; }
+    }
+}
---
+++ CatsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using PurrfectStayAPI;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CatsController : ControllerBase
+{
+    private Database db = new Database();
+
+    [HttpGet]
+    public IActionResult GetCats()
+    {
+        List<Cat> cats = new List<Cat>();
+
+        using (var conn = db.GetConnection())
+        {
+            conn.Open();
+
+            string query = "SELECT * FROM cats";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                cats.Add(new Cat
+                {
+                    id = reader.GetInt32("id"),
+                    name = reader.GetString("name"),
+                    breed = reader.GetString("breed"),
+                    age_years = reader.GetInt32("age_years"),
+                    gender = reader.GetString("gender"),
+                    personality = reader.GetString("personality"),
+                    photo = reader.IsDBNull(reader.GetOrdinal("photo")) ? "" : reader.GetString("photo"),
+                    health_status = reader.GetString("health_status"),
+                    adoption_status = reader.GetString("adoption_status")
+                });
+            }
+        }
+
+        return Ok(cats);
+    }
+}
